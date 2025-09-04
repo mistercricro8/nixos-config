5cfa82dc965ca74a09f256f289af43657a0078c7
mistercricro8 <mistercricro8@gmail.com>
2025-09-04 23:36:12 +0000

    TEST: it works nowgit add .

--- hosts/cricro-vm/configuration.nix
+++ hosts/cricro-vm/configuration.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix    
-    (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
+    (fetchTarball {
+      url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
+      sha256 = "sha256:1rdn70jrg5mxmkkrpy2xk8lydmlc707sk0zb35426v1yxxka10by";
+    })
   ];
 
   services.vscode-server.enable = true;
