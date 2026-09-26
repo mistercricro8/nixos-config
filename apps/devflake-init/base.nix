0ccda10dedb8826f8b7d14286148b40af2faca76
mistercricro8 <mistercricro8@gmail.com>
2026-09-26 16:09:32 -0500

    chore: remove sandbox from devflake init

--- apps/devflake-init/base.nix
+++ apps/devflake-init/base.nix
@@ -5,13 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    sandbox = {
-      url = "github:mistercricro8/nixos-config?dir=extra";
-      inputs = {
-        nixpkgs.follows = "nixpkgs-unstable";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
   outputs =
@@ -19,7 +12,6 @@
       nixpkgs,
       nixpkgs-unstable,
       flake-utils,
-      sandbox,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -46,19 +38,12 @@
           libxcb
           libglvnd
         ];
-        sandboxEnv = sandbox.lib.mkSandbox {
-          inherit pkgs;
-          sandboxPkgs = shellPkgs;
-          runtimeLibs = ldPkgs;
-          hostAllowedBins = [ "agy" "docker" ];
-        };
       in
       {
         devShells.default = pkgs.mkShell {
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ldPkgs;
           packages = pkgs.lib.flatten [
             shellPkgs
-            [ sandboxEnv.runner ]
           ];
           shellHook = "";
           buildInputs = [ pkgs.bashInteractive ];
